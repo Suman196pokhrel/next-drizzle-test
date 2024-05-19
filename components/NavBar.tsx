778d6be29abfeba34efdc8c039c9c4f193182f0d
Suman Pokhrel <spokhrel196@gmail.com>
2024-05-20 01:29:36 +0545

    nav style changes

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 const NavBar = () => {
     return (
-        <nav className="border-2  p-4 rounded-lg fixed w-full z-10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/40">
-            <div className="text-4xl font-bold text-black">TODO_APP</div>
+        <nav className="border-2 p-4 rounded-lg sticky w-full z-10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/40">
+            <div className="text-4xl font-bold text-black">NEXT_DRIZZLE_TODO</div>
         </nav>
     )
 }

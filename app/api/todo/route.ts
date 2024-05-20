a6c5eefdfea864520c787ceb9662f5f64e776175
Suman Pokhrel <spokhrel196@gmail.com>
2024-05-20 14:39:11 +0545

    added additional mock route handlers

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,7 +1,8 @@
 import { db } from "@/lib/db";
 import { TodosTable } from "@/lib/db/schema";
 
-export async function GET(request: Request) {
+// FETCH ALL TODOS
+export async function GET() {
   try {
     const todos = await db.select().from(TodosTable);
     return new Response(JSON.stringify(todos), {
@@ -18,3 +19,12 @@ export async function GET(request: Request) {
     });
   }
 }
+
+// CREATE NEW TODO
+export async function POST() {}
+
+// UPDATE EXISTING TODO
+export async function PUT() {}
+
+// DELETE A TODO
+export async function DELETE() {}
